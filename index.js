f745fde33eb8c0b775ac52d87d12ff9e7bc12140
Anderson <anderson@tresna.co>
2018-03-08 18:28:15 -0300

    fix issue

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import * as settings from './src/settings'
 import dothraki from './src/index'
 
-export { settings, dothraki }
+// export { settings, dothraki }
 
-export default dothraki
+module.exports.settings = dothraki
+module.exports.dothraki = dothraki
+module.exports.default = dothraki
